d4a4b808275f1d4cd1adacfaabb7c03d6bef5cca
kingseungil <109774037+kingseungil@users.noreply.github.com>
2023-02-14 15:49:18 +0900

    ✨ 로그인, 유저정보 가져오기 임시

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -37,8 +37,8 @@ export class UsersController {
      */
     @Post()
     async login(@Body() dto: UserLoginDto): Promise<string> {
-        console.log(dto);
-        return;
+        const { email, password } = dto;
+        return await this.usersService.login(email, password);
     }
 
     /**
@@ -48,7 +48,6 @@ export class UsersController {
      */
     @Get('/:id')
     async getUserInfo(@Param('id') userId: string): Promise<UserInfo> {
-        console.log(userId);
-        return;
+        return await this.usersService.getUserInfo(userId);
     }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,3 +1,4 @@
+import { UserInfo } from './dto/user-info.dto';
 import { EmailService } from './../email/email.service';
 import { Injectable } from '@nestjs/common';
 import * as uuid from 'uuid';
@@ -15,18 +16,33 @@ export class UsersService {
         await this.emailService.sendMemberJoinVerification(email, signupVerifyToken);
     }
     async createUser(name: string, email: string, password: string) {
-        await this.checkUserExists(email);
-
         const signupVerifyToken = uuid.v1();
 
+        // FIXME: await Promise.all 로 바꾸기?
+        await this.checkUserExists(email);
         await this.saveUser(name, email, password, signupVerifyToken);
         await this.sendMemberJoinEmail(email, signupVerifyToken);
     }
     async verifyEmail(signupVerifyToken): Promise<string> {
-        // TODO
+        // TODO: DB, JWT 연동 후 구현
         // 1. DB에서 인증토큰으로 회원 가입 처리중인 유저있는지 조회하고 없다면 에러처리
         // 2. 바로 로그인 상태가 되도록 JWT발급
 
         throw new Error('Method not implemented');
+    }
+    async login(email, password): Promise<string> {
+        // TODO: DB, JWT 연동 후 구현
+        // 1. email, password 를 가진 유저가 존재하는지 DB에서 확인 후 없다면 에러처리
+        // 2. JWT발급
+
+        throw new Error('Method not  implemented');
+    }
+
+    async getUserInfo(userId: string): Promise<UserInfo> {
+        // TODO: DB,JWT 연동 후 구현
+        // 1. userId를 가진 유저가 존재하는지 DB에서 확인 후 없다면 에러처리
+        // 2. 조회된 데이터를 UserInfo타입으로 응답(리턴)
+
+        throw new Error('Method not  implemented');
     }
 }
